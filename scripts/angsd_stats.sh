0b3cc4d1eebdeb3dd5337b18201bde0fef243e8e
JacksonRidges <86210796+JacksonRidges@users.noreply.github.com>
2025-10-23 15:54:11 -0600

    Update angsd_stats.sh

--- scripts/angsd_stats.sh
+++ scripts/angsd_stats.sh
@@ -12,7 +12,7 @@ win_size="50000"
 win_step="5000"
 
 cd ${directory}
-angsd -P 8 -bam ${bam_list} -doSaf 1 -out ${run_name} -anc ${ref} -GL 2
+angsd -P 8 -bam ${bam_list} -doSaf 1 -out ${run_name} -anc ${ref} -GL 2 -minMapQ 30 -minQ 20
 realSFS ${run_name}.saf.idx -P 24 -fold 1 > ${run_name}.sfs
 realSFS saf2theta ${run_name}.saf.idx -outname ${run_name} -sfs ${run_name}.sfs -fold 1
 thetaStat do_stat ${run_name}.thetas.idx -win ${win_size} -step ${win_step} -outnames ${run_name}.thetasWindow.gz
